3625d76ae820bd2ea4b0724b04f376d8469fc09d
Liana Chin <liana.chin@runads.com>
2016-06-21 14:12:11 -0400

    resolve syntax errors

--- lib/capistrano/slack.rb
+++ lib/capistrano/slack.rb
@@ -7,88 +7,87 @@ require 'active_support/all'
 module Capistrano
   module Slack
 
-      def payload(announcement)
-      {
-        'channel' => fetch(:slack_room),
-        'username' => fetch(:slack_username, ''),
-        'text' => announcement,
-        'icon_emoji' => fetch(:slack_emoji, ''),
-        'parse' => fetch(:slack_parse, '')
-        }.to_json
-      end
+    def payload(announcement)
+    {
+      'channel' => fetch(:slack_room),
+      'username' => fetch(:slack_username, ''),
+      'text' => announcement,
+      'icon_emoji' => fetch(:slack_emoji, ''),
+      'parse' => fetch(:slack_parse, '')
+      }.to_json
+    end
 
-      def slack_webhook_url
-        fetch(:slack_webhook_url)
-      end
+    def slack_webhook_url
+      fetch(:slack_webhook_url)
+    end
 
-      def slack_connect(message)
-        begin
-          uri = URI.parse(slack_webhook_url)
-          http = Net::HTTP.new(uri.host, uri.port)
-          http.use_ssl = true
-          http.verify_mode = OpenSSL::SSL::VERIFY_PEER
-          request = Net::HTTP::Post.new(uri.request_uri)
-          request.set_form_data(:payload => payload(message))
-          http.request(request)
-        rescue SocketError => e
-           puts "#{e.message} or slack may be down"
-         end
-      end
+    def slack_connect(message)
+      begin
+        uri = URI.parse(slack_webhook_url)
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+        request = Net::HTTP::Post.new(uri.request_uri)
+        request.set_form_data(:payload => payload(message))
+        http.request(request)
+      rescue SocketError => e
+         puts "#{e.message} or slack may be down"
+       end
+    end
 
-      def slack_defaults
-        if fetch(:slack_deploy_defaults, true) == true
-          before 'deploy', 'slack:starting'
-          before 'deploy:migrations', 'slack:starting'
-          after 'deploy', 'slack:finished'
-          after 'deploy:migrations', 'slack:finished'
+    def slack_defaults
+      if fetch(:slack_deploy_defaults, true) == true
+        before 'deploy', 'slack:starting'
+        before 'deploy:migrations', 'slack:starting'
+        after 'deploy', 'slack:finished'
+        after 'deploy:migrations', 'slack:finished'
+      end
+    end
+
+    def self.extended(configuration)
+      configuration.load do
+
+        before('deploy') do
+          slack_defaults
         end
-      end
 
-      def self.extended(configuration)
-        configuration.load do
+        set :deployer do
+          ENV['GIT_AUTHOR_NAME'] || `git config user.name`.chomp
+        end
 
-          before('deploy') do
-            slack_defaults
+        namespace :slack do
+          task :starting do
+            msg = "Revision #{fetch(:current_revision, fetch(:branch))} of "\
+                "#{fetch(:application)} is deploying to #{fetch(:stage)} by #{fetch(:deployer)}"
+            end
+
+            slack_connect(msg)
+            set(:start_time, Time.now)
           end
 
-          set :deployer do
-            ENV['GIT_AUTHOR_NAME'] || `git config user.name`.chomp
-          end
-
-          namespace :slack do
-            task :starting do
+          task :finished do
+            begin
               msg = "Revision #{fetch(:current_revision, fetch(:branch))} of "\
-                  "#{fetch(:application)} is deploying to #{fetch(:stage)} by #{fetch(:deployer)}"
+                      "#{fetch(:application)} finished deploying to #{fetch(:stage)} by #{fetch(:deployer)}"
+              if start_time = fetch(:start_time, nil)
+                elapsed = Time.now.to_i - start_time.to_i
+                msg << " in #{elapsed} seconds."
+              else
+                msg << "."
               end
-
               slack_connect(msg)
-              set(:start_time, Time.now)
-            end
-
-            task :finished do
-              begin
-                announced_deployer = fetch(:deployer)
-                msg = "Revision #{fetch(:current_revision, fetch(:branch))} of "\
-                        "#{fetch(:application)} finished deploying to #{fetch(:stage)} by #{fetch(:deployer)}"
-                if start_time = fetch(:start_time, nil)
-                  elapsed = Time.now.to_i - start_time.to_i
-                  msg << " in #{elapsed} seconds."
-                else
-                  msg << "."
-                end
-                slack_connect(msg)
-              end
-            end
-
-            task :failed do
-              msg = "#{fetch(:deployer)}'s deployment of #{fetch(:current_revision, fetch(:branch))} to #{fetch(:stage)} failed"
-            end
-
-            task :cancelled do
-              msg = "#{fetch(:deployer)} cancelled deployment of #{fetch(:current_revision, fetch(:branch))} to #{fetch(:stage)}"
             end
           end
 
+          task :failed do
+            msg = "#{fetch(:deployer)}'s deployment of #{fetch(:current_revision, fetch(:branch))} to #{fetch(:stage)} failed"
+            slack_connect(msg)
+          end
+
+          task :cancelled do
+            msg = "#{fetch(:deployer)} cancelled deployment of #{fetch(:current_revision, fetch(:branch))} to #{fetch(:stage)}"
+            slack_connect(msg)
+          end
         end
     end
   end
