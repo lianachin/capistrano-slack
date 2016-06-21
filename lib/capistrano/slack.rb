2d7cae5a5d0e5169f69e07239f1f9afdffe86be8
Liana Chin <liana.chin@runads.com>
2016-06-21 18:08:53 -0400

    remove extra text line

--- lib/capistrano/slack.rb
+++ lib/capistrano/slack.rb
@@ -53,7 +53,6 @@ module Capistrano
       {
         'channel' => fetch(:slack_room),
         'username' => fetch(:slack_username, ''),
-        'text' => announcement,
         'icon_emoji' => fetch(:slack_emoji, ''),
         'parse' => fetch(:slack_parse, ''),
         'attachments' => [{
