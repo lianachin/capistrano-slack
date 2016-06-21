dfb77aa0431790268efca8978a507c93913c59b1
Liana Chin <liana.chin@runads.com>
2016-06-21 15:26:48 -0400

    fix finished task

--- lib/capistrano/slack.rb
+++ lib/capistrano/slack.rb
@@ -51,19 +51,14 @@ module Capistrano
           slack_defaults
         end
 
-        set :deployer do
-          ENV['GIT_AUTHOR_NAME'] || `git config user.name`.chomp
-        end
-
         namespace :slack do
           task :starting do
-            slack_connect "Revision #{fetch(:current_revision)} of #{fetch(:application)} is deploying to #{fetch(:stage)} by #{fetch(:deployer)}"
-            end
+            slack_connect "Revision #{fetch(:revision)} of #{fetch(:application)} is deploying to #{fetch(:stage)} by #{fetch(:deployer)}"
           end
 
           task :finished do
             begin
-              msg = "Revision #{fetch(:current_revision)} of #{fetch(:application)} finished deploying to #{fetch(:stage)} by #{fetch(:deployer)}"
+              msg = "Revision #{fetch(:revision)} of #{fetch(:application)} finished deploying to #{fetch(:stage)} by #{fetch(:deployer)}"
               if start_time = fetch(:start_time, nil)
                 elapsed = Time.now.to_i - start_time.to_i
                 msg << " in #{elapsed} seconds."
@@ -75,12 +70,12 @@ module Capistrano
           end
 
           task :failed do
-            msg = "#{fetch(:deployer)}'s deployment of #{fetch(:current_revision)} to #{fetch(:stage)} failed"
+            msg = "#{fetch(:deployer)}'s deployment of #{fetch(:revision)} to #{fetch(:stage)} failed"
             slack_connect(msg)
           end
 
           task :cancelled do
-            msg = "#{fetch(:deployer)} cancelled deployment of #{fetch(:current_revision)} to #{fetch(:stage)}"
+            msg = "#{fetch(:deployer)} cancelled deployment of #{fetch(:revision)} to #{fetch(:stage)}"
             slack_connect(msg)
           end
         end
